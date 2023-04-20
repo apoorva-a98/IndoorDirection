662d2081418d8052099f34f92fc388535b41c46d
Apoorva Avadhana <apoorvaavadhana@gmail.com>
2023-04-20 02:56:08 -0400

    Ading Manifest and ading a sensor read file
    sensor read seems sketchy

---
+++ Magnetometer/app/src/main/java/com/example/magnetometer/SensorRead.kt
@@ -0,0 +1,29 @@
+package me.privatepractice.hellofirebase
+
+import android.hardware.SensorManager
+import android.hardware.Sensor
+import android.util.Log
+
+
+// To isolate the log output so that only statements in this class are shown type "tag=:SensorRead" in the filter
+private const val TAG = "SensorRead"
+
+/*Function for Identifying Sensors
+https://developer.android.com/guide/topics/sensors/sensors_overview#sensors-identify
+ */
+fun listSensors(sensorManager: SensorManager){
+    /*List all of the Sensors on this device
+     returns a List that contains the type 'Sensor'
+     */
+    val deviceSensors: List<Sensor> = sensorManager.getSensorList(Sensor.TYPE_ALL)
+    /*
+    Use the List's Iterator method to write all the sensors to the log
+    .forEach method uses 'it' to represent the value at the iterator's index
+     https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections/for-each.html#foreach
+     */
+    deviceSensors.forEach {
+        // the '$' inside the quotes allows one to insert a variable into the string
+        // String Templates - https://kotlinlang.org/docs/strings.html#string-templates
+        Log.d(TAG, "Device Sensor:$it ")
+    }
+}
