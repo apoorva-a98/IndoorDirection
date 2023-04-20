b9c314b6bccbfee869fa597b3b02a47d09630b1c
Apoorva Avadhana <apoorvaavadhana@gmail.com>
2023-04-20 02:19:43 -0400

    Late Initialization
    // lateinit https://kotlinlang.org/docs/properties.html#late-initialized-properties-and-variables

--- Magnetometer/app/src/main/java/com/example/magnetometer/MainActivity.kt
+++ Magnetometer/app/src/main/java/com/example/magnetometer/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.magnetometer
 
+import android.hardware.SensorManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -12,6 +13,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.magnetometer.ui.theme.MagnetometerTheme
 
+private val TAG = "MAIN"
+private lateinit var  sensorManager: SensorManager
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
