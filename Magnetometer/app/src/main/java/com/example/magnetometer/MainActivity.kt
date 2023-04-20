fe53c0648d0b9015850811a3e90f8d69c886f35e
Apoorva Avadhana <apoorvaavadhana@gmail.com>
2023-04-20 02:22:01 -0400

    Listing Available sensors on device

--- Magnetometer/app/src/main/java/com/example/magnetometer/MainActivity.kt
+++ Magnetometer/app/src/main/java/com/example/magnetometer/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.magnetometer
 
+import android.content.Context
+import android.hardware.Sensor
 import android.hardware.SensorManager
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -18,6 +21,25 @@ private lateinit var  sensorManager: SensorManager
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Flow for Identifiying Sensors
+        // https://developer.android.com/guide/topics/sensors/sensors_overview#sensors-identify
+        // Initialize the SensorManager
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+        //List all of the Sensors on this device
+        // returns a List that contains the type 'Sensor'
+        val deviceSensors: List<Sensor> = sensorManager.getSensorList(Sensor.TYPE_ALL)
+        // Use the List's Iterator method to write all the sensors to the log
+        // .forEach method uses 'it' to represent the value at the iterator's index
+        // https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections/for-each.html#foreach
+
+        deviceSensors.forEach {
+            // the '$' inside the quotes allows one to insert a variable into the string
+            // String Templates - https://kotlinlang.org/docs/strings.html#string-templates
+            Log.d(TAG, "Device Sensor:$it ")
+        }
+
         setContent {
             MagnetometerTheme {
                 // A surface container using the 'background' color from the theme
